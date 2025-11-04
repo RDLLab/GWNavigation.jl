6914df0a8914ffbf4d1f8c852993b085e3acabf5
Yohan <yohanjayasanka0044@gmail.com>
2025-11-04 21:50:25 +1100

    Particle post processor

--- scripts/visualization.jl
+++ scripts/visualization.jl
@@ -5,14 +5,15 @@ using POMDPTools
 using GLMakie
 
 # Example usage
-pomdp = GWNavigationPOMDP(grid_size=60) # Default 20x20 GridWorld
+pomdp = GWNavigationPOMDP(grid_size=20) # Default 20x20 GridWorld
 policy = GWAStarPolicy(pomdp; uniform_weight=0.1)
 
 # #Visualize A* policy
 # GWNavigation.plot_astar_policy(pomdp, policy)
 
-updater = BootstrapFilter(pomdp, 1000)
-simulator = GWNavigationSimulator(max_steps=30)
+updater = BootstrapFilter(pomdp, 1000, postprocess=GWNavigationParticlePostProcessor(pomdp))
+simulator = GWNavigationSimulator(max_steps=60)
+# simulator = HistoryRecorder(max_steps=60)
 
 simulate(simulator, pomdp, policy, updater)
 
--- src/GWNavigation.jl
+++ src/GWNavigation.jl
@@ -3,10 +3,11 @@ module GWNavigation
 using DataStructures
 using POMDPs
 using POMDPTools
+using ParticleFilters
 using Random
 using StaticArrays
 
-export GWNavigationPOMDP, GWState, GWObservation, GWAStarPolicy, GWNavigationSimulator
+export GWNavigationPOMDP, GWState, GWObservation, GWAStarPolicy, GWNavigationSimulator, GWNavigationParticlePostProcessor
 
 const GWState = SVector{2, Int}  # (x, y) coordinates   Type alias for state representation
 
@@ -95,10 +96,10 @@ end
 # Transition model
 function POMDPs.transition(pomdp::GWNavigationPOMDP, s::GWState, a::Symbol)
     if POMDPs.isterminal(pomdp, s)
-        return SparseCat([s], [1.0])
+        return Deterministic(s)
     elseif haskey(pomdp.goal_states, s) || haskey(pomdp.danger_states, s)
         # Transition to terminal state from goal or danger states
-        return SparseCat([GWTerminalState], [1.0])
+        return Deterministic(GWTerminalState)
     end
 
     intended_state = move(s, a, pomdp.grid_size)
@@ -171,7 +172,7 @@ function POMDPs.observation(pomdp::GWNavigationPOMDP, sp::GWState)
         return Deterministic(GWTerminalObservation)
     else
         # Null observation (no information)
-        return SparseCat([GWNullObservation], [1.0])
+        return Deterministic(GWNullObservation)
     end
 end
 
@@ -219,5 +220,6 @@ end
 include("constructors.jl")
 include("policy.jl")
 include("visualization.jl")
+include("particle_filtering.jl")
 
 end # module GWNavigation
